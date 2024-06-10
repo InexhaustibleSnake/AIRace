0020d7d3986396926d85557d09f3edc600f859fe
InexhaustibleSnake <guchmazoval@gmail.com>
2024-06-10 16:52:31 +0300

    Added random movement speed and bot name display

--- Source/AIRace/Private/Characters/AIRAICharacter.cpp
+++ Source/AIRace/Private/Characters/AIRAICharacter.cpp
@@ -1,4 +1,42 @@
 // This project is made for a test assignment
 
 #include "Characters/AIRAICharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/TextRenderComponent.h"
 
+AAIRAICharacter::AAIRAICharacter()
+{
+    BotNameText = CreateDefaultSubobject<UTextRenderComponent>("BotNameText");
+    BotNameText->SetupAttachment(GetRootComponent());
+
+    FVector BotNameTextLocation(0.0f, 0.0f, 100.0f);
+    BotNameText->SetRelativeLocation(BotNameTextLocation);
+    BotNameText->HorizontalAlignment = EHorizTextAligment::EHTA_Center;
+    BotNameText->WorldSize = 10.0f;
+}
+
+void AAIRAICharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (RandomMovementSpeed)
+    {
+        SetMovementSpeed(FMath::RandRange(MovementSpeedRange.GetLowerBoundValue(), MovementSpeedRange.GetUpperBoundValue()));
+    }
+
+    SetBotNameText(FText::FromString(GetName()));
+}
+
+void AAIRAICharacter::SetMovementSpeed(float NewMovementSpeed)
+{
+    if (!GetCharacterMovement() || !HasAuthority()) return;
+
+    GetCharacterMovement()->MaxWalkSpeed = NewMovementSpeed;
+}
+
+void AAIRAICharacter::SetBotNameText(const FText& NewText)
+{
+    if (!BotNameText) return;
+
+    BotNameText->SetText(NewText);
+}
--- Source/AIRace/Public/Characters/AIRAICharacter.h
+++ Source/AIRace/Public/Characters/AIRAICharacter.h
@@ -6,9 +6,29 @@
 #include "Characters/AIRBaseCharacter.h"
 #include "AIRAICharacter.generated.h"
 
+class UTextRenderComponent;
+
 UCLASS()
 class AIRACE_API AAIRAICharacter : public AAIRBaseCharacter
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AAIRAICharacter();
+
+protected:
+    virtual void BeginPlay() override;
+
+    void SetMovementSpeed(float NewMovementSpeed);
+
+    void SetBotNameText(const FText& NewText);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    bool RandomMovementSpeed = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (EditCondition = "RandomMovementSpeed"))
+    FFloatRange MovementSpeedRange = FFloatRange(400.0f, 700.0f);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    UTextRenderComponent* BotNameText;
 };
